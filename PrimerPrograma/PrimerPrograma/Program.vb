7d3669120e998f2d96355473115f96ccb69d4e2f
KarenMorales16 <karen.morales16@tectijuana.edu.mx>
2021-04-19 21:59:31 -0700

    Metodo Iterativo

---
+++ PrimerPrograma/PrimerPrograma/Program.vb
@@ -0,0 +1,10 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        'Este es mi comentario
+        Console.WriteLine("Mi primer Programa en Visual Basic . NET ")
+        Console.ReadKey(True)
+
+    End Sub
+End Module
---
+++ Variables, Constantes y tipos de datos/Variables,Constantesytipodedatos/Program.vb
@@ -0,0 +1,15 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim mivariable As Integer
+        Const Constante As Double = 16.55
+        mivariable = 0
+
+        Console.WriteLine("mi variable vale" + mivariable)
+        Console.WriteLine("Mi constante vale" + Constante)
+        Console.ReadKey(True)
+
+
+    End Sub
+End Module
